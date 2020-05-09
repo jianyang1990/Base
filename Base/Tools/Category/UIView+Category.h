ad823f9d10bf3f60209c40b3f35960674add83c2
yangjian <yangjian>
2020-05-09 11:29:54 +0800

    feat:增加常用category

---
+++ Base/Tools/Category/UIView+Category.h
@@ -0,0 +1,51 @@
+//
+//  PK-ios
+//
+//  Created by peikua on 15/9/15.
+//  Copyright (c) 2015年 peikua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Category)
+
+@property (nonatomic, assign, readwrite) CGFloat originX;
+@property (nonatomic, assign, readonly) CGFloat rightX;
+@property (nonatomic, assign, readwrite) CGFloat originY;
+@property (nonatomic, assign, readonly) CGFloat bottomY;
+@property (nonatomic, assign, readwrite) CGFloat centerX;
+@property (nonatomic, assign, readwrite) CGFloat centerY;
+@property (nonatomic, assign, readwrite) CGFloat width;
+@property (nonatomic, assign, readwrite) CGFloat height;
+@property (nonatomic, assign, readwrite) CGSize size;
+
+//把View加在Window上
+- (void) addToWindow;
+
+@end
+
+@interface UIView (Screenshot)
+
+//View截图
+- (UIImage*) screenshot;
+
+//ScrollView截图 contentOffset
+- (UIImage*) screenshotForScrollViewWithContentOffset:(CGPoint)contentOffset;
+
+//View按Rect截图
+- (UIImage*) screenshotInFrame:(CGRect)frame;
+
+//整个view转成图片
+- (UIImage*) convertToImage;
+@end
+
+@interface UIView (Animation)
+
+//左右抖动动画
+- (void) shakeAnimation;
+
+//旋转180度
+- (void) trans180DegreeAnimation;
+
+@end
+
---
+++ Base/Tools/Category/UIView+Category.m
@@ -0,0 +1,185 @@
+//
+//  PK-ios
+//
+//  Created by peikua on 15/9/15.
+//  Copyright (c) 2015年 peikua. All rights reserved.
+//
+
+#import "UIView+Category.h"
+#import <QuartzCore/QuartzCore.h>
+#import <objc/runtime.h>
+
+@implementation UIView (Category)
+
+- (void)addToWindow {
+    id appDelegate = [[UIApplication sharedApplication] delegate];
+    if ([appDelegate respondsToSelector:@selector(window)]) {
+        UIWindow *window = (UIWindow *)[appDelegate performSelector:@selector(window)];
+        [window addSubview:self];
+    }
+}
+
+- (CGFloat)originX {
+    return self.frame.origin.x;
+}
+
+- (void)setOriginX:(CGFloat)originX {
+    CGRect frame = self.frame;
+    frame.origin.x = originX;
+    self.frame = frame;
+}
+
+- (CGFloat)rightX {
+    return self.frame.origin.x + self.frame.size.width;
+}
+
+- (CGFloat)originY {
+    return self.frame.origin.y;
+}
+
+- (void)setOriginY:(CGFloat)originY {
+    CGRect frame = self.frame;
+    frame.origin.y = originY;
+    self.frame = frame;
+}
+
+- (CGFloat)bottomY {
+    return self.frame.origin.y + self.frame.size.height;
+}
+
+- (CGFloat)centerX {
+    return self.center.x;
+}
+
+- (void)setCenterX:(CGFloat)centerX {
+    CGPoint center = self.center;
+    center.x = centerX;
+    self.center = center;
+}
+
+- (CGFloat)centerY {
+    return self.center.y;
+}
+
+- (void)setCenterY:(CGFloat)centerY {
+    CGPoint center = self.center;
+    center.y = centerY;
+    self.center = center;
+}
+
+- (CGFloat)width {
+    return self.frame.size.width;
+}
+
+- (void)setWidth:(CGFloat)width {
+    CGRect frame = self.frame;
+    frame.size.width = width;
+    self.frame = frame;
+}
+
+- (CGFloat)height {
+    return self.frame.size.height;
+}
+
+- (void)setHeight:(CGFloat)height {
+    CGRect frame = self.frame;
+    frame.size.height = height;
+    self.frame = frame;
+}
+
+- (CGSize)size {
+    return self.frame.size;
+}
+
+- (void)setSize:(CGSize)size {
+    CGRect frame = self.frame;
+    frame.size = size;
+    self.frame = frame;
+}
+
+@end
+
+@implementation UIView (Screenshot)
+
+- (UIImage *)screenshot {
+    UIGraphicsBeginImageContext(self.bounds.size);
+    [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    // helps w/ our colors when blurring
+    // feel free to adjust jpeg quality (lower = higher perf)
+    NSData *imageData = UIImageJPEGRepresentation(image, 0.75);
+    image = [UIImage imageWithData:imageData];
+
+    return image;
+}
+
+- (UIImage *)screenshotForScrollViewWithContentOffset:(CGPoint)contentOffset {
+    UIGraphicsBeginImageContext(self.bounds.size);
+    //need to translate the context down to the current visible portion of the scrollview
+    CGContextTranslateCTM(UIGraphicsGetCurrentContext(), 0.0f, -contentOffset.y);
+    [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    // helps w/ our colors when blurring
+    // feel free to adjust jpeg quality (lower = higher perf)
+    NSData *imageData = UIImageJPEGRepresentation(image, 0.55);
+    image = [UIImage imageWithData:imageData];
+
+    return image;
+}
+
+- (UIImage *)screenshotInFrame:(CGRect)frame {
+    UIGraphicsBeginImageContext(frame.size);
+    CGContextTranslateCTM(UIGraphicsGetCurrentContext(), frame.origin.x, frame.origin.y);
+    [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    // helps w/ our colors when blurring
+    // feel free to adjust jpeg quality (lower = higher perf)
+    NSData *imageData = UIImageJPEGRepresentation(image, 0.75);
+    image = [UIImage imageWithData:imageData];
+
+    return image;
+}
+
+- (UIImage *)convertToImage {
+    CGSize viewSize = self.bounds.size;
+    UIGraphicsBeginImageContextWithOptions(viewSize, NO, [UIScreen mainScreen].scale);
+    [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
+@end
+
+@implementation UIView (Animation)
+
+- (void)shakeAnimation {
+    CALayer *layer = [self layer];
+    CGPoint position = [layer position];
+    CGPoint y = CGPointMake(position.x - 8.0f, position.y);
+    CGPoint x = CGPointMake(position.x + 8.0f, position.y);
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position"];
+    [animation setTimingFunction:[CAMediaTimingFunction
+                                  functionWithName:kCAMediaTimingFunctionEaseInEaseOut]];
+    [animation setFromValue:[NSValue valueWithCGPoint:x]];
+    [animation setToValue:[NSValue valueWithCGPoint:y]];
+    [animation setAutoreverses:YES];
+    [animation setDuration:0.08f];
+    [animation setRepeatCount:3];
+    [layer addAnimation:animation forKey:nil];
+}
+
+- (void)trans180DegreeAnimation
+{
+    [UIView animateWithDuration:0.3 animations:^{
+        self.layer.transform = CATransform3DMakeRotation(M_PI, 0, 1, 0);
+    }];
+}
+
+@end
